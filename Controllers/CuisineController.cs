4e60fe5ebe4df949a7edbfdc5e5e57112325d2c3
abrightalpha <abright@alphalytics.net>
2015-06-23 13:51:15 -0400

    OdeToFood Project

---
+++ Controllers/CuisineController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using OdeToFood.Filters;
+
+namespace OdeToFood.Controllers
+{
+    [Log]
+    public class CuisineController : Controller
+    {
+        // GET: Cuisine
+       
+        public ActionResult Search(string name)
+        {
+            var message = Server.HtmlEncode(name);
+
+            return Content(message);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        OdeToFoodDb _db = new OdeToFoodDb();
+
+        public ActionResult Index()
+        {
+            var model = _db.Restaurants.ToList();
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            var model = new AboutModel();
+            model.Name = "Bob";
+            model.Location = "Bethesda, Maryland";
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
